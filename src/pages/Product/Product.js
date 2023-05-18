a17924e93b7e67758d72be4aec332a934d116b94
adiMallya <adimallya99@gmail.com>
2023-05-19 03:00:04 +0530

    refact : added Filter & displayed filtered data

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,23 +1,44 @@
 import { useEffect } from "react";
 
 import { useDataContext } from "../../contexts";
+import { getSortedData, getFilteredData } from "../../utils";
 import { ProductCard } from "./components/ProductCard/ProductCard";
+import { Filters } from "./components/Filters/Filters";
 import "./Product.css";
 
 export function Product() {
-  const { products } = useDataContext();
+  const {
+    products,
+    filters: { sortBy, ratingRange, searchValue, inStock, categoryType },
+  } = useDataContext();
+
+  const sortedProducts = getSortedData(products, sortBy);
+  const filteredProducts = getFilteredData(
+    sortedProducts,
+    ratingRange,
+    categoryType,
+    inStock,
+    searchValue
+  );
 
   useEffect(() => {
     document.title = "commerch | products";
   }, []);
 
   return (
-    <main className="products__container">
-      <div className="grid-view">
-        {products.map((product) => (
-          <ProductCard key={product._id} product={product} />
-        ))}
-      </div>
+    <main className="row">
+      <Filters />
+      <section className="products__container grid-view">
+        {filteredProducts.length > 0 ? (
+          filteredProducts.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))
+        ) : (
+          <>
+            <h2>No Products Found</h2>
+          </>
+        )}
+      </section>
     </main>
   );
 }
