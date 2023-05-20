a3fcab229c61a1282db57228d9eb15e1c8e9f7f4
adiMallya <adimallya99@gmail.com>
2023-05-20 12:11:56 +0530

    refact : added functionality to toast

--- src/components/Toast/Toast.jsx
+++ src/components/Toast/Toast.jsx
@@ -1,12 +1,28 @@
+import { useDataContext } from "../../contexts";
+import { ACTION_TYPE } from "../../utils";
+import { useEffect } from "react";
 import "./Toast.css";
 
 export const Toast = (props) => {
-  const { position = "top-right", type = "info" } = props;
+  const { position = "top-right" } = props;
+  const {
+    toast: { type, msg },
+    dispatch,
+  } = useDataContext();
+
+  const closeToast = () => {
+    dispatch({ type: ACTION_TYPE.SHOW_TOAST, payload: null });
+  };
+
+  useEffect(() => {
+    const timeId = setTimeout(closeToast, 2000);
+    return () => clearTimeout(timeId);
+  });
 
   return (
     <div className={`toast ${position} toast-${type} `}>
-      {"This is success!"}
-      <button className="btn-close"></button>
+      {msg}
+      <button className="btn-close" onClick={close}></button>
     </div>
   );
 };
