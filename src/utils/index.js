3ae96290d71af395d2443405d2981551c4a3876e
adiMallya <adimallya99@gmail.com>
2023-05-21 19:53:45 +0530

    add : array utils for cart & wishlist

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,4 @@
 export { ACTION_TYPE } from "./constant";
 export { getSortedData, getFilteredData } from "./filterData";
+export { getDiscountPercentage } from "./getPriceDetails";
+export { isItemInCart, isItemInWishlist } from "./arrayUtils";
