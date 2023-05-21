7b9092de5bf2bc275573e71a33ee6e333e6c5e2d
adiMallya <adimallya99@gmail.com>
2023-05-21 23:15:50 +0530

    add : Wishlist page, move/remove items

---
+++ src/pages/Wishlist/Wishlist.js
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+import { useDataContext } from "../../contexts";
+import "./Wishlist.css";
+import { WishlistCard } from "./WishlistCard";
+import { useEffect } from "react";
+export function Wishlist() {
+  const { wishlist } = useDataContext();
+
+  const wishlistIsNotEmpty = wishlist.length > 0;
+
+  useEffect(() => {
+    document.title = "commerch | wishlist";
+  }, []);
+
+  return (
+    <main className="page">
+      <div
+        className="vertical-middle-self display-flex page-heading"
+        role="heading"
+      >
+        <h3>My Wishlist </h3>
+        <span className="item-count">
+          {wishlistIsNotEmpty && `${wishlist.length} items`}
+        </span>
+      </div>
+      {wishlistIsNotEmpty ? (
+        <section className="wishlist__container">
+          {wishlist.map((item) => (
+            <WishlistCard key={item._id} product={item} />
+          ))}
+        </section>
+      ) : (
+        <div className="vertical-middle-self">
+          <p>Your Wishlist is Empty</p>
+          <Link className="btn btn--primary-outline" to="/products">
+            Explore Products
+          </Link>
+        </div>
+      )}
+    </main>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { Product } from "./Product/Product";
 export { Login, SignUp } from "./Auth";
 export { Profile } from "./Profile/Profile";
+export { Wishlist } from "./Wishlist/Wishlist";
