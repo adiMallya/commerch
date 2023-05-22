bc6754169e06c70e9ac21595dcc6a241aa32cda7
adiMallya <adimallya99@gmail.com>
2023-05-23 03:58:52 +0530

    refact : added basic detail page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-export { Product } from "./Product/Product";
+export { ProductList, ProductDetail } from "./Product";
 export { Login, SignUp } from "./Auth";
 export { Profile } from "./Profile/Profile";
 export { Wishlist } from "./Wishlist/Wishlist";
