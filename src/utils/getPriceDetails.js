7fbd8239a59db9cef38fa68e848db89b352888be
adiMallya <adimallya99@gmail.com>
2023-05-23 02:30:20 +0530

    chore : added util for cart price details

--- src/utils/getPriceDetails.js
+++ src/utils/getPriceDetails.js
@@ -2,3 +2,14 @@
 export function getDiscountPercentage(price, originalPrice) {
   return Math.floor(Math.abs((price / originalPrice) * 100 - 100));
 }
+// method to calculate cart price
+export function getCartValues(cart) {
+  return cart.reduce(
+    ({ cartPrice, discount }, { price, originalPrice, qty }) => {
+      cartPrice += originalPrice * qty;
+      discount += (originalPrice - price) * qty;
+      return { cartPrice, discount };
+    },
+    { cartPrice: 0, discount: 0 }
+  );
+}
