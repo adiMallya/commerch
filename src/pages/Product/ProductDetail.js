abd51117e3796450bb5bd57fe3694670d9f52255
adiMallya <adimallya99@gmail.com>
2023-05-23 03:59:15 +0530

    add : created basic ProductDetail page(WIP)

---
+++ src/pages/Product/ProductDetail.js
@@ -0,0 +1,105 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { RiShoppingBag2Fill } from "react-icons/ri";
+import { FaHeart, FaStar } from "react-icons/fa";
+import { useAuthContext, useDataContext } from "../../contexts";
+import {
+  getDiscountPercentage,
+  isItemInCart,
+  isItemInWishlist,
+} from "../../utils";
+import { addToCart, addToWishlist } from "../../services";
+
+export function ProductDetail() {
+  const { productId } = useParams();
+  const navigate = useNavigate();
+  const {
+    user: { token },
+  } = useAuthContext();
+  const { products, cart, wishlist, dispatch } = useDataContext();
+
+  const product = products?.find((product) => product._id === productId);
+
+  const percentageOff = getDiscountPercentage(
+    product?.price,
+    product?.originalPrice
+  );
+
+  const inCart = isItemInCart(product?._id, cart);
+  const inWishlist = isItemInWishlist(product?._id, wishlist);
+
+  const addToCartHandler = () => {
+    token
+      ? inCart
+        ? navigate("/cart")
+        : addToCart(token, product, dispatch)
+      : navigate("/login");
+  };
+
+  const moveToWishlistHandler = () => {
+    token
+      ? inWishlist
+        ? navigate("/wishlist")
+        : addToWishlist(token, product, dispatch)
+      : navigate("/login");
+  };
+
+  return (
+    <main className="page">
+      <div className="product-detail__container">
+        <div key={product?._id} className="card-horizontal">
+          <div className="image-container badge-container">
+            <img src={product?.img} alt={product?._id} className="img-res" />
+            {product?.onSale && (
+              <span className="badge bg-secondary badge--sale">On Sale</span>
+            )}
+          </div>
+          <div className="card-info">
+            <div className="card-header">
+              <div>
+                <p className="card-brand">{product?.brand}</p>
+                <p className="card-desc">{product?.name}</p>
+              </div>
+            </div>
+            <div className="card-price">
+              <span className="disc-price">₹ {product?.price}</span>
+              <span className="orig-price">₹ {product?.originalPrice}</span>
+              <span className="disc-percent">{`(${percentageOff}% OFF)`}</span>
+            </div>
+            <div className="cta-container">
+              <button
+                className="btn btn--primary-text-icon"
+                onClick={addToCartHandler}
+              >
+                {inCart ? (
+                  <span role="button" className="btn-icon">
+                    {" "}
+                    Go To Bag
+                  </span>
+                ) : (
+                  <span role="button" className="btn-icon">
+                    <RiShoppingBag2Fill /> Add To Bag
+                  </span>
+                )}
+              </button>
+              <button
+                className="btn btn--secondary-outline"
+                onClick={moveToWishlistHandler}
+              >
+                {inWishlist ? (
+                  <span role="button" className="btn-icon">
+                    {" "}
+                    Go To Wishlist
+                  </span>
+                ) : (
+                  <span role="button" className="btn-icon">
+                    <FaHeart /> Add To Wishlist
+                  </span>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
