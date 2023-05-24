ed483420ac41bc656b05952ea8c3fccf45646366
adiMallya <adimallya99@gmail.com>
2023-05-25 02:41:09 +0530

    add : service for address calls

---
+++ src/services/address.service.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { ACTION_TYPE } from "../utils";
+
+export const addNewAddress = async (encodedToken, address, dispatch) => {
+  try {
+    const {
+      status,
+      data: { address },
+    } = await axios.post(
+      "/api/user/address",
+      { address },
+      { headers: { authorization: encodedToken } }
+    );
+    if (status === 201)
+      dispatch({ type: ACTION_TYPE.SET_ADDRESS, payload: address });
+  } catch (error) {
+    console.error(error);
+    dispatch({
+      type: ACTION_TYPE.SHOW_ERROR,
+      payload: "Error is adding new address",
+    });
+  }
+};
+
+export const removeAddress = async (addressId, encodedToken, dispatch) => {
+  try {
+    const {
+      status,
+      data: { address },
+    } = await axios.post(`/api/user/address/${addressId}`, {
+      headers: { authorization: encodedToken },
+    });
+    if (status === 200)
+      dispatch({ type: ACTION_TYPE.SET_ADDRESS, payload: address });
+  } catch (error) {
+    console.error(error);
+    dispatch({
+      type: ACTION_TYPE.SHOW_ERROR,
+      payload: "Error is deleting address",
+    });
+  }
+};
