620f0a6b31bd245ecdb45b6121abed0ac842488e
adiMallya <adimallya99@gmail.com>
2023-05-25 02:44:13 +0530

    chore : made profile component

---
+++ src/pages/Account/components/UserProfile/UserProfile.jsx
@@ -0,0 +1,27 @@
+import { useAuthContext } from "../../../../contexts";
+
+export const UserProfile = () => {
+  const {
+    user: { user },
+    logOutUser,
+  } = useAuthContext();
+
+  const { firstName, lastName, email } = user;
+  return (
+    <>
+      <div className="profile-details">
+        <div className="col">
+          <span role="label">Name</span>
+          <span role="label">Email</span>
+        </div>
+        <div className="col">
+          <span>{`${firstName} ${lastName}`}</span>
+          <span>{email}</span>
+        </div>
+      </div>
+      <button className="btn btn--secondary-outline" onClick={logOutUser}>
+        Log Out
+      </button>
+    </>
+  );
+};
