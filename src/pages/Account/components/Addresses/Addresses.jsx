4f56662d839b7a22d03523c9695b6e72dcb17026
adiMallya <adimallya99@gmail.com>
2023-05-25 14:25:34 +0530

    add : Address list

--- src/pages/Account/components/Addresses/Addresses.jsx
+++ src/pages/Account/components/Addresses/Addresses.jsx
@@ -11,35 +11,43 @@ export const Addresses = () => {
 
   const [showModal, setShowModal] = useState(false);
 
-  const addNewBtnHandler = () => setShowModal(true);
-
   return (
     <>
-      {address && (
-        <ul className="list list--style-none">
-          {address.map(({ _id, name, street, city, state, pincode, phno }) => (
-            <li key={_id} className="address__container">
-              <p>{name}</p>
-              <div>
-                <p>{`${street},`}</p>
-                <p>{`${city} - ${pincode}, ${state}`}</p>
-                <p>Mobile: {`+91 ${phno}`}</p>
-              </div>
-              <div className="">
-                <button
-                  className="btn btn--secondary-outline"
-                  onClick={() => removeAddress(_id, token, dispatch)}
-                >
-                  Remove
-                </button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      )}
-      <button className="btn btn--primary-outline" onClick={addNewBtnHandler}>
+      <button
+        className="btn btn--primary-outline"
+        onClick={() => setShowModal(true)}
+      >
         + Add New Address
       </button>
+      {address && (
+        <ul className="list list--style-none">
+          {address.map(
+            ({ _id, name, street, city, state, pincode, phno, isDefault }) => (
+              <li key={_id} className="address__container">
+                <div className="address-header">
+                  <p>{name}</p>
+                  {isDefault && (
+                    <span className="badge bg-primary">Default</span>
+                  )}
+                </div>
+                <div>
+                  <p>{`${street},`}</p>
+                  <p>{`${city} - ${pincode}, ${state}`}</p>
+                  <p>Mobile: {`+91 ${phno}`}</p>
+                </div>
+                <div className="">
+                  <button
+                    className="btn btn--secondary-outline"
+                    onClick={() => removeAddress(_id, token, dispatch)}
+                  >
+                    Remove
+                  </button>
+                </div>
+              </li>
+            )
+          )}
+        </ul>
+      )}
       <AddressForm showForm={showModal} closeForm={() => setShowModal(false)} />
     </>
   );
