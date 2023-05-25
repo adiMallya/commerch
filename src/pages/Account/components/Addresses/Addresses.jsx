8a30df300ffbff9169d935d40e40a6a538f38dd5
adiMallya <adimallya99@gmail.com>
2023-05-26 01:16:34 +0530

    styles : styled address container

--- src/pages/Account/components/Addresses/Addresses.jsx
+++ src/pages/Account/components/Addresses/Addresses.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useAuthContext, useDataContext } from "../../../../contexts";
 import { removeAddress } from "../../../../services";
 import { AddressForm } from "./AddressForm";
+import "./Address.css";
 
 export const Addresses = () => {
   const {
@@ -21,31 +22,26 @@ export const Addresses = () => {
       </button>
       {address && (
         <ul className="list list--style-none">
-          {address.map(
-            ({ _id, name, street, city, state, pincode, phno, isDefault }) => (
-              <li key={_id} className="address__container">
-                <div className="address-header">
-                  <p>{name}</p>
-                  {isDefault && (
-                    <span className="badge bg-primary">Default</span>
-                  )}
-                </div>
-                <div>
-                  <p>{`${street},`}</p>
-                  <p>{`${city} - ${pincode}, ${state}`}</p>
-                  <p>Mobile: {`+91 ${phno}`}</p>
-                </div>
-                <div className="">
-                  <button
-                    className="btn btn--secondary-outline"
-                    onClick={() => removeAddress(_id, token, dispatch)}
-                  >
-                    Remove
-                  </button>
-                </div>
-              </li>
-            )
-          )}
+          {address.map(({ _id, name, street, city, state, pincode, phno }) => (
+            <li key={_id} className="address__container">
+              <div className="address-header">
+                <p>{name}</p>
+              </div>
+              <div>
+                <p>{`${street},`}</p>
+                <p>{`${city} - ${pincode}, ${state}`}</p>
+                <p>Mobile: {`+91 ${phno}`}</p>
+              </div>
+              <div className="display-flex">
+                <button
+                  className="btn btn--secondary-outline"
+                  onClick={() => removeAddress(_id, token, dispatch)}
+                >
+                  Remove
+                </button>
+              </div>
+            </li>
+          ))}
         </ul>
       )}
       <AddressForm showForm={showModal} closeForm={() => setShowModal(false)} />
