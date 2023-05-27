a1ee7cc473d744be092c601d01f59b5100467eef
adiMallya <adimallya99@gmail.com>
2023-05-27 17:32:01 +0530

    add : Checkout price component

---
+++ src/pages/Checkout/components/CheckoutPrice.jsx
@@ -0,0 +1,42 @@
+export const CheckoutPrice = ({ cart, priceDetails }) => {
+  const { cartPrice, discount, convenienceFee, totalAmt } = priceDetails;
+
+  return (
+    <section className="checkout__container--item price-card">
+      <div className="price-card--title" role="heading">
+        <span>Price Details</span>
+        <span className="">{`(${cart.length} ${
+          cart.length > 1 ? "items" : "item"
+        })`}</span>
+      </div>
+      <div className="price-details">
+        <div className="row">
+          <span role="label">Total MRP</span>
+          <span className="price-mrp">{`₹${cartPrice}`}</span>
+        </div>
+        <div className="row">
+          <span role="label">Discount on MRP</span>
+          <span className="price-discount">- {discount}</span>
+        </div>
+        <div className="row">
+          <span role="label">Convenience Fee</span>
+          <span className="price-ship">
+            <span className={convenienceFee === 0 ? "strike-price" : ""}>
+              ₹99
+            </span>{" "}
+            {convenienceFee === 0 && <span className="price-free">FREE</span>}
+          </span>
+        </div>
+      </div>
+      <div className="row total-amt">
+        <span role="label">Total Amount</span>
+        <span className="price-total">{`₹${totalAmt}`}</span>
+      </div>
+      <div className="cta-container">
+        <button className="btn btn--primary-solid checkout-btn">
+          Continue
+        </button>
+      </div>
+    </section>
+  );
+};
