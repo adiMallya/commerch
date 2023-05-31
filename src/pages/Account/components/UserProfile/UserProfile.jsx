e7f0841a226f6e550a3c980fabc41064915dd35d
adiMallya <adimallya99@gmail.com>
2023-05-31 13:12:50 +0530

    refact : log out handler

--- src/pages/Account/components/UserProfile/UserProfile.jsx
+++ src/pages/Account/components/UserProfile/UserProfile.jsx
@@ -1,13 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "../../../../contexts";
+import { ACTION_TYPE } from "../../../../utils";
 import "./UserProfile.css";
 
 export const UserProfile = () => {
-  const {
-    user: { user },
-    logOutUser,
-  } = useAuthContext();
+  const navigate = useNavigate();
+  const { user, dispatch } = useAuthContext();
 
   const { firstName, lastName, email } = user;
+
+  const logOutUser = () => {
+    dispatch({ type: ACTION_TYPE.LOG_OUT });
+    navigate("/");
+  };
+
   return (
     <>
       <div className="profile-details">
