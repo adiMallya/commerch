bfa6634af8d33a2163a9caf48be9743080b87732
adiMallya <adimallya99@gmail.com>
2023-05-31 09:37:35 +0530

    refact : added featured images

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -10,15 +10,18 @@ export const categories = [
     _id: uuid(),
     categoryName: "Hoodies",
     description: "Layer up with stylish hoodies.",
+    img: "https://source.unsplash.com/nBaf7c_wd80",
   },
   {
     _id: uuid(),
     categoryName: "Accessories",
     description: "Unleash your accessory obsession!",
+    img: "https://source.unsplash.com/-Gtecp6zmeU",
   },
   {
     _id: uuid(),
     categoryName: "Posters",
     description: "Unleash your inner collector's vibe!",
+    img: "https://source.unsplash.com/wMkaMXTJjlQ",
   },
 ];
